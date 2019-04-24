ecbcd122048d0bd064be856b3ce322f3b1e578a3
Amir Edin <amiredin@yahoo.com>
2019-04-24 17:32:18 +0300

    ms

--- config.py
+++ config.py
@@ -26,7 +26,6 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-    pass
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
 
 class DevConfig(Config):

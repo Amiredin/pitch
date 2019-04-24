0b87e6c0b7149bb0b27144f48e394e28b6458f2c
Amir Edin <amiredin@yahoo.com>
2019-04-24 16:04:24 +0300

    deployemnt

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
@@ -29,7 +29,6 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-    pass
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
 
 class DevConfig(Config):
@@ -39,7 +38,6 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-   
     DEBUG = True
 
 class TestConfig(Config):

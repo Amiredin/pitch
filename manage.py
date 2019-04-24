35231203d979cc527a71246504a51b813fd7438b
Amir Edin <amiredin@yahoo.com>
2019-04-24 16:44:35 +0300

    deply

--- manage.py
+++ manage.py
@@ -12,9 +12,7 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app, db = db, User = User, Pitch = Pitch)
+
 
 @manager.command
 def test():
@@ -24,6 +22,12 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Pitch = Pitch)
+
+
 
 if __name__=='__main__':
     manager.run()
